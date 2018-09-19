ce3156b353c09c9e574295c287526449a331cf5e
Jonathan Simms <jsimms@jsimms-ltm4.internal.salesforce.com>
2018-09-19 19:19:11 -0300

    First Commit

---
+++ Heroku Attempt/src/lesson1/MyClass.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Entere a number");
+		int a_number = scan.nextInt();
+		System.out.println("The value is");
+		System.out.println(a_number);
+
+	}
+
+}
